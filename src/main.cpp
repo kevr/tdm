ffab7b18d1e008f12b5c85d1c7421552d1dc1b46
Kevin Morris <kevr@0cost.org>
2024-01-18 15:50:22 -0800

    clang-format: reformat codebase to align with style
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ using std::endl;
 
 #define PROJECT_NAME "tdm"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     if (argc != 1) {
         cerr << std::format("{} takes no arguments.", argv[0]) << endl;
         return 1;
